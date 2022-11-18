662fb0b00954e5e8b0f57946702165d6b4774f46
Carles Gallardo <carles.gallardo00@gmail.com>
2022-11-18 12:35:57 +0100

    Changed Debug print method to OutputDebugString, refactored structs.hpp to utils.hpp, deleted debug.hpp

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -3,22 +3,24 @@
 //
 #pragma once
 
+#include <debugapi.h>
+#include <string>
+
 namespace trk{
     struct Vector3{
         float x;
         float y;
         float z;
 
-        static Vector3 zero(){
-            return Vector3{0,0,0};
-        };
+        static Vector3 zero();
     };
 
     struct Tracker{
         uint32_t id;
         Vector3 pos;
-        void updatePosition(Vector3 newPos){
-            pos = newPos;
-        }
+        void updatePosition(Vector3 newPos);
+
     };
+
+    void debugPrint(std::string msg);
 }
